dff2efd2f7a59560289cd200f85975bcc1e93caa
David Kuehn <kuehnd96@gmail.com>
2015-09-23 19:36:08 -0500

    Recreated UWP UI project. It builds and runs on the phone!

---
+++ Store/DK.UOME.Store.UI.UWP/DesignData/DesignEntryViewModel.cs
@@ -0,0 +1,32 @@
+﻿using DK.UOME.Store.UI.DataModel;
+using DK.UOME.Store.PresentationModel.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DK.UOME.Store.UI.UWP.DesignData
+{
+    public class DesignEntryViewModel : EntryViewModel
+    {
+        public DesignEntryViewModel()
+        {
+            DateTime today = DateTime.Now.Date;
+            
+            Entry movieEntry = new BorrowedEntry()
+            {
+                CreateDate = today.AddDays(-22),
+                DueDate = today.AddDays(3),
+                Id = 1,
+                OtherParty = "Jon Kuehn",
+                Thing = "Despicable Me 2 Blu Ray",
+                Note = "Return insert, too."
+            };
+
+            Entry = movieEntry;
+
+            Label = "Borrowed Entry";
+        }
+    }
+}
---
+++ Store/DK.UOME.Store.UI.UWP/Views/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using DK.UOME.Store.UI.UWP.DesignData;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DK.UOME.Store.UI.UWP.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+#if DEBUG
+            this.DataContext = new DesignMainViewModel();
+#endif
+        }
+
+        private void OnMenuButtonClick(object sender, RoutedEventArgs e)
+        {
+            SplitView.IsPaneOpen = !SplitView.IsPaneOpen;
+        }
+
+        private void OnNavListSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SplitView.IsPaneOpen = false;
+        }
+
+        private void OnNavListLoaded(object sender, RoutedEventArgs e)
+        {
+            //NOTE: Why does this run twice?
+
+            var listView = sender as ListView;
+
+            if ((listView != null) &&
+                (listView.Items.Any()))
+            {
+                listView.SelectedIndex = 0;
+            }
+        }
+    }
+}
